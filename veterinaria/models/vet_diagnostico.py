8afab2c22e96b8ff4ee9bc5791dfac124e92e0cd
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-03-24 03:18:47 -0500

    av

--- veterinaria/models/vet_diagnostico.py
+++ veterinaria/models/vet_diagnostico.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
 class Diagnostico(models.Model):
-    _name = "veterinaria.diagnostico"
+    _name = "ficha.diagnostico"
 
     name = fields.Char(string="Name", translate=True)
     descripcion = fields.Text(string="Descripcion", translate=True)
