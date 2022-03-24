c818c88c850db4852c4e86c07ca12789c037311b
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-03-24 04:31:18 -0500

    a

--- veterinaria/__manifest__.py
+++ veterinaria/__manifest__.py
@@ -27,8 +27,8 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/vet_diagnostico.xml',
-        'views/menu.xml',
         'security/security.xml',
+        'views/menu.xml',
         # 'security/security2.xml',
     ],
     # only loaded in demonstration mode
