b4e7bcb4dcadab8c533d0cf620077915c26b3b14
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2022-03-27 18:40:31 -0500

    a

--- veterinaria/models/veterinaria.py
+++ veterinaria/models/veterinaria.py
@@ -12,7 +12,7 @@ class Veterinaria(models.Model):
     nombre = fields.Char()
     raza = fields.Char(string="Raza")
     diagnostico_id = fields.Many2one("ficha.diagnostico", string="Ficha de Diagnostico")
-    imagen = fields.Binary(
+    imagen = fields.Image(
         attachment=True, help="Agrega la foto de tu animal."
     )
     
